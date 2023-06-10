674f7e830956afecb31995cefa5eaf522d240251
Tomás <89207931+tomasClavijo@users.noreply.github.com>
2023-06-10 03:08:05 -0300

    Ejercicio 6 - Completo

---
+++ Ejercicio 6/ejercicio6.cpp
@@ -0,0 +1,54 @@
+#include <cassert>
+#include <string>
+#include <iostream>
+#include <limits>
+using namespace std;
+
+bool esImpar (int n){
+    if (n % 2 == 0){
+        return false;
+    } else{
+        return true;
+    }
+}
+
+int elementoNoRepetido(int * arr, int i, int f){
+        if (i == f){
+        return arr[i];
+    }
+
+    int mitad = (f + i) / 2;
+
+    bool impar;
+
+    if (arr[mitad] == arr[mitad + 1]){
+        impar = esImpar(mitad);
+        if (impar){
+            elementoNoRepetido(arr, i, mitad -1);
+        } else{
+            elementoNoRepetido(arr, mitad, f);
+        }
+    } else {
+        impar = esImpar(mitad + 1);
+        if (impar){
+            elementoNoRepetido(arr, i, mitad);
+        } else{
+            elementoNoRepetido(arr, mitad + 1, f);
+        }
+    }
+}
+
+int main(){
+    int cantidadElementos;
+    cin >> cantidadElementos;
+    int* arr = new int[cantidadElementos];
+    int elemento;
+    for (int i = 0; i < cantidadElementos; i++){
+        cin >> elemento;
+        arr[i] = elemento;
+    }
+    int noRepetido = elementoNoRepetido(arr, 0, cantidadElementos - 1);
+    cout << noRepetido << endl;
+    delete [] arr;
+    return 0;
+}
